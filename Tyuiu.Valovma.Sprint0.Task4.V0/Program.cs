cbbad43e23f91db1bb2e39606ab041c936e3be9c
xeweles <maxval0v1@gmail.com>
2026-09-10 20:41:25 +0500

    Task4

---
+++ Tyuiu.Valovma.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,9 @@
+﻿using Tyuiu.Valovma.Sprint0.Task4.V0.Lib;
+
+Console.WriteLine(DataService.Addition(1, 5));
+Console.WriteLine(DataService.Subtraction(10, 5));
+Console.WriteLine(DataService.Multiplication(10, 10));
+Console.WriteLine(DataService.Division(10, 5));
+
+
+Console.ReadKey();
