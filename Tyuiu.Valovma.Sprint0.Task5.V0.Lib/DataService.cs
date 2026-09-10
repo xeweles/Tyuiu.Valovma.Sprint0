594b5b2af5949f22e621a9d1a98a37033acb4cdd
xeweles <maxval0v1@gmail.com>
2026-09-10 20:59:31 +0500

    Task5

---
+++ Tyuiu.Valovma.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,32 @@
+﻿namespace Tyuiu.Valovma.Sprint0.Task5.V0.Lib;
+
+public class DataService
+{
+    public static int Addition(int a, int b)
+    {
+        return a + b;
+    }
+    
+    public static int Subtraction(int a, int b)
+    {
+        return a - b;
+    }
+    
+    public static int Multiplication(int a, int b)
+    {
+        return a * b;
+    }
+    
+    public static double Division(int a, int b)
+    {
+        if (b == 0)
+        {
+            Console.WriteLine("b = {0}", b);
+            return -1;
+        }
+        else
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Tyuiu.Valovma.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,8 @@
+﻿using Tyuiu.Valovma.Sprint0.Task5.V0.Lib;
+
+
+Console.WriteLine("A + B = " + DataService.Addition(1, 5));
+Console.WriteLine("A - B = " + DataService.Subtraction(15, 5));
+Console.WriteLine("A * B = " + DataService.Multiplication(15, 5));
+Console.WriteLine("A / B = " + DataService.Division(5, 0));
+Console.ReadKey();
