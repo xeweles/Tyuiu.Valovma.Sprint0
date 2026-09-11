96143a2bf2b1c56180f124fd06975f04c7caeda6
xeweles <maxval0v1@gmail.com>
2026-09-11 19:44:59 +0500

    Task6

---
+++ Tyuiu.Valovma.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,30 @@
+namespace Tyuiu.Valovma.Sprint0.Task6.V0.Test;
+
+using Tyuiu.Valovma.Sprint0.Task6.V0.Lib;
+
+[TestClass]
+public class DataServiceTest
+{
+    [TestMethod]
+    public void CheckAdditionArrayValid()
+    {
+        var numbers = new int[] { 1, 2, 3, 4, 5 };
+        var res = DataService.AdditionArray(numbers);
+        Assert.AreEqual(15, res);
+    }
+    [TestMethod]
+    public void CheckSubstractionArrayValid()
+    {
+        var numbers = new int[] { 1, 2, 3, 4, 5 };
+        var res = DataService.SubtractionArray(numbers);
+        Assert.AreEqual(-15, res);
+    }
+
+    [TestMethod]
+    public void CheckMultArrayValid()
+    {
+        var numbers = new int[] { 1, 2, 3, 4, 5 };
+        var res = DataService.MultiplicationArray(numbers);
+        Assert.AreEqual(120, res);
+    }
+}
---
+++ Tyuiu.Valovma.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.Valovma.Sprint0.Task6.V0.Lib;
+
+
+int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+
+//Пример циклической структуры находится в библиотеке классов в методе AdditionArray
+Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(numsArray));
+
+//Пример циклической структуры находится в библиотеке классов в методе SubtractionArray
+Console.WriteLine("Разность элементов массива = " + DataService.SubtractionArray(numsArray));
+
+//Пример циклической структуры находится в библиотеке классов в методе MultiplicationArray
+Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(numsArray));
+
+Console.ReadKey();
